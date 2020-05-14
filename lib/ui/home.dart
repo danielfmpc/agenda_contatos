9bf863db949dca710b259163d5b72cce6b8e754e
Daniel Fernando <daniel_fmpc@hotmail.com.br>
2020-05-14 03:50:19 -0300

    Criando a estrutura e listando com card os contatos

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:agenda_contatos/helpers/contact_helper.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -6,10 +9,95 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  ContactHelper helper = ContactHelper();
+  List<Contact> contacts = List();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();    
+    helper.getAllContacts().then((list) {
+      setState(() {
+        contacts = list;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Contatos"),
+        backgroundColor: Colors.red,
+        centerTitle: true,
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.red,
+        child: Icon(Icons.add),
+        onPressed: (){},
+      ),
+      body: ListView.builder(
+        padding: EdgeInsets.all(10),
+        itemCount: contacts.length,
+        itemBuilder: (context, index){
+          return _contactCard(context, index);
+        },
+      ),
+    );
+  }
+  Widget _contactCard(BuildContext context, int index){
+    return GestureDetector(
+      onTap: (){},
+      child: Card(
+        
+        child: Padding(
+          padding: EdgeInsets.all(10),
+          child: Row(
+            children: <Widget>[
+               Container(
+                width: 80,
+                height: 80,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    image: contacts[index].img != null ?
+                      FileImage(File(contacts[index].img)) :
+                      AssetImage("images/persong.png")
+                  ),
+                ),                
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      contacts[index].name ?? "",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 22
+                      ),
+                    ),
+                    Text(
+                      contacts[index].email ?? "",
+                      style: TextStyle(
+                        fontSize: 18
+                      ),
+                    ),
+                    Text(
+                      contacts[index].phone ?? "",
+                      style: TextStyle(
+                        fontSize: 18
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
